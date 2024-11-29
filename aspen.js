bfa25ab4063dceb602b2c2f400bcc9b2dcde7ccc
jadennpn <jadennpye@gmail.com>
2024-11-28 19:08:32 -0700

    Add files via upload

---
+++ aspen.js
@@ -0,0 +1,71 @@
+twone.addEventListener('click',function(){
+    title.innerHTML = "2021 Silly Cat"
+    console.log(2021);
+    document.body.classList.remove("home");
+    document.body.classList.remove("twtwo");
+    document.body.classList.remove("twthree");
+    document.body.classList.remove("twfour");
+    document.body.classList.add("twone");
+    document.getElementById('aspen2020').src="Aspen2021.png";
+    document.getElementById('aspen2020').alt="2021 aspen";
+    document.getElementById("pmain").innerHTML= "Look at 2021 Aspen, what did she see? I’ll tell you what.. Who remembers that was 3 years ago! But I bet she saw me and just got so excited she forgot to keep her cool and that’s why she made such a goofy face! 2021 was a simple year for us, I went back to work after covid so Aspen was always extra excited when I would come home!";
+    document.getElementById("h2title").innerHTML = "The Year She Was Happy To See Me";
+    document.getElementById ("p1").innerHTML = "Beautiful Aspen turned 5 years old in 2021!!!"
+})
+twtwo.addEventListener('click',function(){
+    title.innerHTML = "2022 New Friends..."
+    console.log(2022);
+    document.body.classList.remove("home");
+    document.body.classList.remove("twone");
+    document.body.classList.remove("twthree");
+    document.body.classList.remove("twfour");
+    document.body.classList.add("twtwo");
+    document.getElementById('aspen2020').src="Aspen2022.jpg";
+    document.getElementById('aspen2020').alt="2022 aspen";
+    document.getElementById("pmain").innerHTML= "Uh Oh…… Who’s this? 2022 was a life changing year for Aspen, we had moved into a new apartment with a new room mate, his name is Puppy. Aspen and Puppy never got along, or at least Aspen didn’t.. Puppy is a super sweet cat but he was so shy, he let Aspen beat him up a little and eat some of his food! Aspen was a bad kitty in 2022, but like I said before, she’s so cute we let her get away with it!";
+    document.getElementById("h2title").innerHTML ="The Year She Made An Enemy";
+    document.getElementById ("p1").innerHTML = "Aspen might not be as friendly as she looks..";
+    
+})
+twthree.addEventListener('click',function(){
+    title.innerHTML = "2023 Hungry Kitty"
+    console.log(2023);
+    document.body.classList.remove("home");
+    document.body.classList.remove("twtwo");
+    document.body.classList.remove("twone");
+    document.body.classList.remove("twfour");
+    document.body.classList.add("twthree");
+    document.getElementById('aspen2020').src="Aspen2023.png";
+    document.getElementById('aspen2020').alt="2023 aspen";
+    document.getElementById("pmain").innerHTML= "This is my favourite and least favourite photo at the same time… I don’t think she’s ever looked uglier than she does here LOL! Poor Aspen, a friend was over one day and while cooking dinner, he snapped this ugly picture of Aspen, I think she really wanted that food.. No other explanation for such an expression..";
+    document.getElementById("h2title").innerHTML = "The Year She Was Really Ugly";
+    document.getElementById ("p1").innerHTML = "This may be her worst, sorry you all have to see this.";
+})
+twfour.addEventListener('click',function(){
+    title.innerHTML = "2024 Even Hungrier Kitty"
+    console.log(2021);
+    document.body.classList.remove("home");
+    document.body.classList.remove("twtwo");
+    document.body.classList.remove("twthree");
+    document.body.classList.remove("twone");
+    document.body.classList.add("twfour");
+    document.getElementById('aspen2020').src="Aspen2024.jpg";
+    document.getElementById('aspen2020').alt="2024 aspen";
+    document.getElementById("pmain").innerHTML= "I think I was too mean to Aspen the past 3 years… But she makes it too easy. Here’s her being CRAZY and trying to eat some human food. Got some A&W burgers and she had her eye on them all night. Luckily I at them before she did! 2024 has been a wonderful year for me and Aspen, she comfort me during my breakup and while I struggled at school, she is my biggest inspiration and my best friend! I love you Aspen, no matter how ugly or how stupid you may be!";
+    document.getElementById("h2title").innerHTML ="Aspen As We Know And Love Today";
+    document.getElementById ("p1").innerHTML = "My beautiful angel cat, forever and always my baby you’ll be.";
+})
+home.addEventListener('click',function(){
+    title.innerHTML = "Aspens Stupidest Moments 2k20-2k24"
+    console.log(2021);
+    document.body.classList.remove("twone");
+    document.body.classList.remove("twtwo");
+    document.body.classList.remove("twthree");
+    document.body.classList.remove("twfour");
+    document.body.classList.add("home");
+    document.getElementById('aspen2020').src="Aspen2020.jpg";
+    document.getElementById('aspen2020').alt="2020 aspen";
+    document.getElementById("pmain").innerHTML= "This image is Aspen in 2020 after she underwent a skull transplant... JUST KIDDING! This silly cat let me take a picture of her, huge mistake since I immediatly made her head a triangle. 2020 was a tough year for me, I had lost a family member and moved away from home. Aspen was always there to cheer me up, even if it meant becoming a triangle. Her best quality is that she never lets me be lonely, she loves to cuddle and play so there's never a dull moment!";
+    document.getElementById("h2title").innerHTML = "Welcome to a showcase of my cat Aspen through the past 4 years!";
+    document.getElementById ("p1").innerHTML = "Aspen has always been a little rascal, she loves climbing up the walls and eating human food! Shes so cute we let her get away with it. But I was left no choice, someone had to make a website to show the world she's actually kind of stupid! So I'll show you the silly stupid pictures of her and talk about what kind of year we had! Enjoy!!";
+})
